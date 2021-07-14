beaf33cb5bb5d08d92b1fdf7c9e1194badcaeb09
liamvinson <liamvinson1@gmail.com>
2021-07-14 11:18:05 +0100

    Convert database uri for heroku

--- config.py
+++ config.py
@@ -3,11 +3,19 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+def process_uri(uri):
+
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+
+    return uri
+
+
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = process_uri(os.environ['DATABASE_URL'])
 
 
 class ProductionConfig(Config):
@@ -17,4 +25,6 @@ class ProductionConfig(Config):
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+
+
 
